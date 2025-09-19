c9527338ae7e4628e4263c40584fd6b72cc73f68
Jayant Chandawni <jayantchandwani05@gmail.com>
2025-09-20 03:21:52 +0530

    Added OR Tools Benchmark Code

---
+++ or_tools_benchmarks.py
@@ -0,0 +1,127 @@
+import json
+import os
+from ortools.constraint_solver import routing_enums_pb2
+from ortools.constraint_solver import pywrapcp
+
+# ----------- CONSTANTS -----------
+DATASET_NO = 0
+CUMULATIVE_DIST = 5000000
+DIST_SLACK = 0
+CAP_SLACK = 0
+TIME_LIMIT = 45
+
+def get_dataset(dataset_no = DATASET_NO):
+    file_path = os.path.join('cached_datasets', f"dataset_{dataset_no}.json")
+    with open(file_path, 'r') as json_file:
+        return json.load(json_file)
+
+def print_solution(data, manager, routing, solution):
+    """Prints solution on console."""
+    print(f"Objective: {solution.ObjectiveValue() / 100:.2f}")
+    total_distance = 0
+    total_load = 0
+    for vehicle_id in range(data["num_vehicles"]):
+        if not routing.IsVehicleUsed(solution, vehicle_id):
+            continue
+        index = routing.Start(vehicle_id)
+        plan_output = f"Route for vehicle {vehicle_id}:\n"
+        route_distance = 0
+        route_load = 0
+        while not routing.IsEnd(index):
+            node_index = manager.IndexToNode(index)
+            route_load += data["demands"][node_index]
+            plan_output += f" {node_index} Load({route_load}) -> "
+            previous_index = index
+            index = solution.Value(routing.NextVar(index))
+            route_distance += routing.GetArcCostForVehicle(
+                previous_index, index, vehicle_id
+            )
+        plan_output += f" {manager.IndexToNode(index)} Load({route_load})\n"
+        plan_output += f"Distance of the route: {route_distance / 100:.2f}m\n"
+        plan_output += f"Load of the route: {route_load}\n"
+        print(plan_output)
+        total_distance += route_distance
+        total_load += route_load
+    print(f"Total distance of all routes: {total_distance / 100:.2f}m")
+    print(f"Total load of all routes: {total_load}")
+    
+
+def find_solution(dataset_no=DATASET_NO):
+    data = get_dataset(dataset_no)
+    
+    manager = pywrapcp.RoutingIndexManager(
+        len(data['distance_matrix']),
+        data['num_vehicles'], 
+        data['starts'],
+        data['ends']
+    )
+    
+    routing = pywrapcp.RoutingModel(manager)
+    
+    # print("Testing IndexToNode calls:")
+    # try:
+    #     for i in range(manager.GetNumberOfIndices()):
+    #         node = manager.IndexToNode(i)
+    #         print(f"  Index {i} -> Node {node}")
+    # except Exception as e:
+    #     print(f"  Error during manual IndexToNode test: {e}")
+    #     return
+    
+    def distance_callback(from_index, to_index):
+        """Returns the distance between the two nodes (handles OR-Tools internal indices)."""
+        # print(f"from_index = {from_index}, to_index = {to_index}")
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return data['distance_matrix'][from_node][to_node]
+
+
+    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+    
+    # dimension_name = "Distance"
+    # routing.AddDimension(
+    #     evaluator_index=transit_callback_index,
+    #     slack_max=DIST_SLACK,
+    #     capacity=CUMULATIVE_DIST,
+    #     fix_start_cumul_to_zero=True,
+    #     name=dimension_name,
+    # )
+    
+    def demand_callback(from_index):
+        """Returns demand for a node, safe against OR-Tools internal indices."""
+        node = manager.IndexToNode(from_index)
+        return data["demands"][node]
+
+    demand_callback_index = routing.RegisterUnaryTransitCallback(demand_callback)
+    routing.AddDimensionWithVehicleCapacity(
+        evaluator_index=demand_callback_index,
+        slack_max=CAP_SLACK,  
+        vehicle_capacities=data["vehicle_capacities"],  
+        fix_start_cumul_to_zero=True,
+        name="Capacity",
+    )
+
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = (
+        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
+    )
+    search_parameters.local_search_metaheuristic = (
+        routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH
+    )
+    search_parameters.time_limit.FromSeconds(TIME_LIMIT)
+
+    solution = routing.SolveWithParameters(search_parameters)
+
+    if solution:
+        print_solution(data, manager, routing, solution)
+    else:
+        print(f"NO SOLUTION exists for the given parameters!!")
+        
+if __name__ == "__main__":
+    
+    print("Dataset :", DATASET_NO)
+    print("Cumulative Dist :", CUMULATIVE_DIST)
+    print("Max Distance Slack:", DIST_SLACK)
+    print("Max Capacity Slack:", CAP_SLACK)
+    
+    find_solution()
---
+++ parse_input.py
@@ -0,0 +1,107 @@
+import re
+import json
+import math
+import os
+# from pprint import pprint, pformat
+
+
+datasets = {}
+
+def euclidian_distance(point1: tuple[int, int], point2: tuple[int, int]) -> float:
+    dist = math.sqrt((abs(point1[0] - point2[0]) ** 2) + (abs(point1[1] - point2[1]) ** 2))
+    return int(dist * 100)
+
+def format_dataset(dataset : dict) -> list[list[int]]:
+    vehicle_locaitons = set(dataset['vehicle_locations'])
+    target_locations = set(dataset['target_locations'])
+    locations = vehicle_locaitons.copy()
+    locations = list(locations.union(target_locations))
+    
+    starts = []
+    ends = []
+    distance_matrix = []
+    for i in range(len(locations)):
+        if locations[i] in vehicle_locaitons:
+            starts.append(i)
+            ends.append(i)
+        distances = []
+        for j in range(len(locations)):
+            distances.append(round(euclidian_distance(locations[i], locations[j]), 2))
+        distance_matrix.append(distances)
+    
+    per_vehicle_cap = (sum(dataset['weights']) + len(starts) - 1) // len(starts)
+    return {
+        "distance_matrix" : distance_matrix,
+        "num_vehicles": len(starts),
+        "demands": dataset['weights'],
+        "vehicle_capacities": [per_vehicle_cap] * len(starts),
+        "starts": starts,
+        "ends": ends
+    }
+        
+def parse_and_save() -> None:
+    """
+    Parses and saves data as json
+    
+    Data in .txt file :
+        Data set #1 
+        Vehicle locations :117,290;120,176;182,177;158,266;140,40;166,17;180,24;35,45;60,113;60,208;
+        Target locations :176,8;90,158;102,83;40,6;173,122;142,24;106,14;169,271;186,70;65,142;165,288;150,58;152,12;54,278;6,228;77,134;31,78;95,293;48,103;24,157;31,52;65,69;39,150;162,128;49,297;112,112;66,29;16,116;12,131;191,104;5,298;32,85;198,57;150,20;163,243;45,5;47,205;176,187;136,249;143,261;94,55;169,134;64,57;183,222;23,253;58,132;165,51;17,15;138,286;140,73;130,151;149,89;119,160;188,194;175,160;108,46;178,85;57,246;133,72;33,104;171,67;91,112;57,61;52,50;175,258;17,16;63,208;56,124;177,22;115,13;195,144;164,139;141,68;109,105;103,10;99,35;39,55;196,23;52,230;20,62;2,43;111,284;9,64;180,139;77,166;185,71;64,193;166,4;180,186;149,111;104,209;193,247;119,78;192,106;66,143;79,294;156,86;58,103;101,221;49,83;
+        Weights = 78,28,100,39,36,50,23,51,48,13,87,71,80,69,40,71,57,11,20,24,86,71,34,39,80,25,40,71,83,94,52,60,78,68,89,49,71,79,34,50,10,41,97,77,13,82,29,77,43,47,85,95,51,92,90,58,33,19,45,61,30,51,62,25,86,97,85,37,83,10,97,90,41,19,69,74,95,80,18,61,17,70,88,44,45,87,21,27,72,92,54,41,59,52,72,36,38,48,60,84
+    saved as :
+        {
+            'distance_matrix' : list[list[int]]
+            'starts' : list[int]
+            'ends' : list[int]
+        }
+    """
+    file_name = 'CVRP_10Vehicles_100Targets.txt'
+    pattern = r"Data set #(\d+)\nVehicle locations :(.*);\nTarget locations :(.*)\nWeights = (.*)"
+    saved = 0
+    
+    cache_dir = 'cached_datasets'
+    os.makedirs(cache_dir, exist_ok=True)
+    
+    with open(file_name, 'r') as input_file:
+        data = input_file.read()
+        
+        matches = re.findall(pattern, data)
+        
+        for match in matches:
+            ds_no = int(match[0])        
+            vehicle_locations_str = match[1]  
+            target_locations_str = match[2]   
+            weights = match[3]   
+            
+            vehicle_locations = [tuple(map(int, s.split(','))) for s in vehicle_locations_str.split(';') if s.strip()]
+            target_locations = [tuple(map(int, s.split(','))) for s in target_locations_str.split(';') if s.strip()]
+            weights = list(map(int, weights.split(',')))
+            
+            dataset = {
+                "vehicle_locations": vehicle_locations,
+                "target_locations": target_locations,
+                "weights": weights
+            }
+            
+            if(ds_no == 1):
+                print(dataset)
+            
+            dataset = format_dataset(dataset)
+
+            json_file_path = os.path.join('cached_datasets', f"dataset_{ds_no}.json")
+            with open(json_file_path, 'w') as json_file:
+                json.dump(dataset, json_file) 
+            saved += 1
+                
+    print(f"Parsed and saved {saved} Datasets")
+
+
+
+if __name__ == "__main__":
+    dataset = {
+        "vehicle_locations" : [(1, 2), (2, 3), (3, 4)],
+        "target_locations" : [(3, 6), (1, 7), (2, 4)]
+    }
+    
+    parse_and_save()
+
